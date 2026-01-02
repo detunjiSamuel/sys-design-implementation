82d2c728645f99c3653b40a96acc3acbeef7652e
Adetunji Samuel <samadetunji01@gmail.com>
2026-01-02 13:52:27 +0000

    init

---
+++ tinylang-interpreter-impl/main.py
@@ -0,0 +1,23 @@
+
+
+
+""""
+
+I will be creating a simple programming language interpreter
+
+
+I will be copying the javascript syntax
+
+Steps
+ Lexa 
+
+ Parse
+
+ Execute
+
+
+""""
+
+
+if __name__ == "__main__":
+  print("Simple programming language interpreter")
